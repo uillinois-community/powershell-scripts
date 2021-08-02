82bc111b48a1a9322cba68d2dee1eaf660a5b49e
Gorman, Jon <jtgorman@illinois.edu>
2021-08-02 11:04:03 -0500

    Made a function to get a mapping of team names to ids

--- InviteAllFromOrg.ps1
+++ InviteAllFromOrg.ps1
@@ -1,25 +1,26 @@
-﻿# the teams stuff is extra sauce that we should probably ask about....
-
+﻿
 #$source_org = Read-Host "Organization Name to use to create invitee list "
 #$target_org = Read-Host "Organization Name to send out invites "
 
-#Set-GitHubAuthentication -SessionOnly
+##Set-GitHubAuthentication -SessionOnly
 
-# step 1: get the users in source org, and each team name they're on in source org
-
-
-
-
+## step 1: get the users in source org, and each team name they're on in source org
+#$source_mapping = Get-GitHubOrg-UserLogins-TeamNames -GitHubOrgName UIUCLibrary
 
 # step 2: get a list of existing users already present in target org, filter out source list so 
 #   we don't re-add those people (TODO - maybe it so that they get added to groups if need be)
+
+#$target_org_members = Get-GitHubOrganizationMember -OrganizationName $target_org | Select -ExpandProperty login
+#$need_to_invite = $source_org_members | where { -not ($already_present -contains $_.login) }
+
+
 # step 3: map the used teams in source to target org, warn if not present and remove/ignore those later
+
+
 # step 4: loop over still not present people, invite them using the proper tem ids
 
 
 
-#$target_org_members = Get-GitHubOrganizationMember -OrganizationName $target_org | Select -ExpandProperty login
-#$need_to_invite = $source_org_members | where { -not ($already_present -contains $_.login) }
 
 #Write-Output $need_to_invite
 
@@ -72,4 +73,36 @@ function Get-GitHubOrg-UserLogins-TeamNames {
 
   return $member_is_part_of
 }
+
+
+# we could be in trouble if there's two teams w/ same name but different ids...
+# should probably check and error 
+function Get-GitHub-Org-Name-To_id {
+
+  param(
+   [Parameter(Mandatory=$true)]
+   $GitHubOrgName 
+  )
+
+  $team_ids = @{} 
+
+  $teams = Get-GitHubTeam -OrganizationName $GitHubOrgName
+
+  foreach( $team in $teams ) {
+    if( $team_ids.ContainsKey( $team.name ) ){
+      throw "There was more than one $team.name in $GitHubOrgName"
+    }
+    else {
+      $team_ids[$team.name] = $team.id
+    }
+  }
+
+  return $team_ids
+}
+
+  
+
+
+
+
 
