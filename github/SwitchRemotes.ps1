a0a5846a6d9a45b691c259efb686ad4bc29f4bd6
Gorman, Jon <jtgorman@illinois.edu>
2021-08-05 17:01:44 -0500

    Adding file SwitchRemotes.ps1, which has currently two functions:
    Find-Local-Repository-Directories, which for a given path will recurse and find all directories containing a .git
    
    Then the started but not completed
    Migrate-Git-Remotes
    
    which currently will take a list of paths and prints out some debugging info...but will go to each repository, rename origin to old-origin and then set the repo to a new target

---
+++ github/SwitchRemotes.ps1
@@ -0,0 +1,45 @@
+﻿#
+# So, currently developing this as a set of useful functions, fire 
+# up ISE and make a list, then edit that list to what you like (should be two seperate scripts int he long run
+
+
+function Find-Local-Repository-Directories {
+
+  param(
+      [Parameter(Mandatory=$true)]
+    $StartingPath
+  )
+
+  return (Get-ChildItem source -Recurse -Directory -Attributes Hidden,!Hidden | Where-Object { $_.Name -eq '.git' } | %{ $_.Parent })
+
+
+}
+
+function Get-Git-Remotes {
+
+  param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$Paths
+  )
+
+  foreach ($path in $paths ) {
+    
+    Set-Location -Path  $path
+    $all_remotes = git remote -v 
+    
+    $origins = $all_remotes | Where { $_ -match '^origin\s' }
+
+    # So, we'll want to update fetch and pull remote urls bh swapping all but the 
+    #
+    # origin	ssh://git@code.library.illinois.edu:7999/ninja/design-directory.git (fetch)
+    # origin	ssh://git@code.library.illinois.edu:7999/ninja/design-directory.git (push)
+
+    foreach ( $remote_target in $origins ) {
+        $naem, $url, $type_raw = $remote_target -split "`t"
+        $type = $type_raw -replace '[()]',''
+        Write-Output "git remote rename origin old-origin"
+    }
+      
+  }
+}
+
