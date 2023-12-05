84beaed7538cd8faaa2c7a74c220aaa921090ab1
Edward Delaporte <delaport@illinois.edu>
2023-12-05 10:30:17 -0600

    Add library to run GitHub CLI commands in multiple repos
    The PowerShellForGitHub module has stopped working for me,
    so I will be migrating my approach.
    
    - Add git clone helper
    - Update Show-AgileNoMilestone to not require PowerShellForGitHub

---
+++ modules/AgileGitHubCLI.psm1
@@ -0,0 +1,58 @@
+<#
+
+.DESCRIPTION
+
+Invoke a GitHub CLI command in every repository configured in $env:GITHUB_REPOS
+
+See https://cli.github.com/manual/
+
+Be sure to set $env:GITHUB_CLONE_PATH and $GITHUB_REPORS,
+and run Invoke-AgileCloneAll first.
+
+.EXAMPLE
+
+Invoke-AgileCmd "gh issue list"
+
+#>
+function Invoke-AgileCmd { 
+    param(
+        [string]$gh_command
+    )
+    $env:GITHUB_REPOS.split(' ') | ForEach-Object {
+		$folder = $_.split('/')[1]
+        $repo_path = "$env:GITHUB_CLONE_PATH/$folder" 
+        cd $repo_path
+		Invoke-Expression $gh_command
+    }
+}
+Export-ModuleMember -Function Invoke-AgileCmd
+
+<#
+
+.DESCRIPTION
+
+Invoke a GitHub CLI command in every repository configured in $env:GITHUB_REPOS
+
+#>
+function Invoke-AgileCloneAll { 
+    param(
+        [string]$gh_command
+    )
+    $env:GITHUB_REPOS.split(' ') | ForEach-Object {
+		$folder = $_.split('/')[1]
+        $repo_path = "$env:GITHUB_CLONE_PATH/$folder" 
+		if(-Not(Test-Path $repo_path)) {
+			cd $env:GITHUB_CLONE_PATH
+			$clone_cmd = "git clone git@github.com:$_"
+			Invoke-Expression $clone_cmd
+		}
+    }
+}
+Export-ModuleMember -Function Invoke-AgileCmd
+Export-ModuleMember -Function Invoke-AgileCloneAll
+
+
+function Show-AgileNoMilestone() {
+	Invoke-AgileCmd "gh issue list -S 'is:open is:issue no:milestone'"
+}
+Export-ModuleMember -Function Show-AgileNoMilestone
